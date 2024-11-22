2d854bb827d812fb7c95bfd3d6a9d99270da7c81
hoanggiang20041 <giang10012004@gmail.com>
2024-11-22 22:03:53 +0700

    Create Student.cs

---
+++ BAITAP_BUOI2_NGUYENHOANGIANG/Student.cs
@@ -0,0 +1,9 @@
+namespace BAITAP_BUOI2_NGUYENHOANGIANG
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
